7e46bbc7ff26ff2ca4519dab49049f407734f0e5
hodorsec <hodorsec@protonmail.com>
2019-03-14 22:38:08 +0100

    Added support for "\x" shellcode input

--- generic/convert_hex_bin.py
+++ generic/convert_hex_bin.py
@@ -4,12 +4,15 @@ import binascii
 if len(sys.argv) != 3:
 	print "Outputs the given HEX encoded string as binary\n"
 	print "Usage: " + sys.argv[0] + " <hex_string> <output_bin>"
-	print "Example: " + sys.argv[0] + " 31c3 example.bin\n"
+	print "Example: " + sys.argv[0] + " \"31c3\" example.bin"
+	print "Example: " + sys.argv[0] + " \"\\x31\\xc3\" example.bin\n"
         exit(0)
 
 payload = sys.argv[1]
 filename = sys.argv[2]
 
+if payload.find("x") != -1:
+    payload = payload.replace("\\x", "")
 payload_bytes = binascii.a2b_hex(''.join(payload))
 
 try:
